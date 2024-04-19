29a04b62d668b1b1f70b506852299b5309f2f982
Flavius Catalin <kete102@gmail.com>
2024-04-19 12:45:33 +0200

    edit db config

--- database/config.js
+++ database/config.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.DB_CNN, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+      useNewUrlParser: true
     })
 
     console.log('DB online')
